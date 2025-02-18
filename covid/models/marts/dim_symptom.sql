7735e393f7056bfaf7586982b7af219c6edb7fda
Victor Moraes <victor.soaresdemoraes@gmail.com>
2025-02-18 17:29:03 -0300

    change surrogate key of dimension tables to be a hash and not an integer

--- covid/models/marts/dim_symptom.sql
+++ covid/models/marts/dim_symptom.sql
@@ -2,16 +2,12 @@ WITH notifications AS (
     SELECT * FROM {{ ref('stg_notifications') }}
 )
 
-SELECT ROW_NUMBER() OVER (ORDER BY SYMPTOM) AS SK_SYMPTOM, * 
-FROM
-    (
-        SELECT DISTINCT 
-            --MD5(TRIM(REPLACE(value, '"', ''))) AS SK_SYMPTOM, 
-            TRIM(REPLACE(value, '"', '')) AS SYMPTOM,
-            GETDATE() AS UPDATE_DATE
-        FROM (
-            SELECT SPLIT(SYMPTOMS, ',') AS SYMPTOMS_ARRAY
-            FROM notifications
-        ) T,
-        LATERAL FLATTEN(INPUT => SYMPTOMS_ARRAY) AS value
-    ) S
+SELECT DISTINCT 
+    MD5(TRIM(REPLACE(value, '"', ''))) AS SK_SYMPTOM, 
+    TRIM(REPLACE(value, '"', '')) AS SYMPTOM,
+    GETDATE() AS UPDATE_DATE
+FROM (
+    SELECT SPLIT(SYMPTOMS, ',') AS SYMPTOMS_ARRAY
+    FROM notifications
+) T,
+LATERAL FLATTEN(INPUT => SYMPTOMS_ARRAY) AS value
--- covid/models/marts/dim_test_type.sql
+++ covid/models/marts/dim_test_type.sql
@@ -2,14 +2,10 @@ WITH notifications AS (
     SELECT * FROM {{ ref('stg_notifications') }}
 )
 
-SELECT ROW_NUMBER() OVER (ORDER BY TEST_TYPE) AS SK_TEST_TYPE, *
-FROM
-    (
-        SELECT DISTINCT 
-            --MD5(TEST_TYPE) AS SK_TEST_TYPE,
-            TEST_TYPE, 
-            '' AS TEST_TYPE_DESCRIPTION,
-            GETDATE() AS UPDATE_DATE
-        FROM notifications
-        WHERE TEST_TYPE IS NOT NULL
-    ) t
+SELECT DISTINCT 
+    MD5(TEST_TYPE) AS SK_TEST_TYPE,
+    TEST_TYPE, 
+    '' AS TEST_TYPE_DESCRIPTION,
+    GETDATE() AS UPDATE_DATE
+FROM notifications
+WHERE TEST_TYPE IS NOT NULL
